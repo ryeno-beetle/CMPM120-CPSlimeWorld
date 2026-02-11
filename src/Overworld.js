a5744bd1520c92810347f0220cdf62b5fe4db1e6
ryeno-beetle <wingytinker@icloud.com>
2026-02-11 15:40:20 -0800

    code practice complete

--- src/Overworld.js
+++ src/Overworld.js
@@ -13,14 +13,44 @@ class Overworld extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         })
+        this.load.image('tilesetImage', 'tileset.png');
+        this.load.tilemapTiledJSON('tilemapJSON', 'overworld.json');
     }
 
     create() {
+        const map = this.add.tilemap('tilemapJSON');
+        const tileset = map.addTilesetImage('tileset', 'tilesetImage');
+        const bgLayer = map.createLayer('Background', tileset, 0, 0);
+        const terrainLayer = map.createLayer('Terrain', tileset, 0, 0);
+        const treeLayer = map.createLayer('Trees', tileset, 0, 0);
+
         // add slime
-        this.slime = this.physics.add.sprite(32, 32, 'slime', 0)
+        const slimeSpawn = map.findObject('Spawns', obj => obj.name === 'slimeSpawn');
+        this.slime = this.physics.add.sprite(slimeSpawn.x, slimeSpawn.y, 'slime', 0); 
         this.slime.body.setCollideWorldBounds(true)
+        
 
         // slime animation
+        this.anims.create({
+            key: 'jiggle',
+            frameRate: 8,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime', {
+                start: 0,
+                end: 1
+            })
+        });
+        this.slime.anims.play('jiggle');
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.slime, true, 0.25, 0.25);
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        terrainLayer.setCollisionByProperty({collides: true});
+        this.physics.add.collider(this.slime, terrainLayer);
+
+        treeLayer.setCollisionByProperty({collides: true});
+        this.physics.add.collider(this.slime, treeLayer);
 
         // input
         this.cursors = this.input.keyboard.createCursorKeys()
